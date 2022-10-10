6cbf691399e23dc99a1c482cdeea8ea97fe83bdd
Paxxous <88162094+Paxxous@users.noreply.github.com>
2022-10-10 14:32:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+  This is javascript made only for your client.
+  Don't poke around here because it's not interacting with any database.
+  So have fun because you aren't going to make any sort of sql injection.
+*/
+
+
+// make the arrow appear when hovering over link
+function showArrow() {
+  const arrowElement = document.getElementById('arrowNavBar');
+  // arrowElement.style.display = 'inline';
+  arrowElement.style.opacity = '100%';
+  arrowElement.className = 'fadeIn'
+}
+
+function hideArrow() {
+  const arrowElement = document.getElementById('arrowNavBar');
+  // arrowElement.style.display = 'none';
+  arrowElement.style.opacity = '0%';
+  arrowElement.className = 'fadeOut'
+}
